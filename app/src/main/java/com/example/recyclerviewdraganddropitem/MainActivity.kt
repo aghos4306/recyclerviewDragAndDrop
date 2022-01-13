69085c5e77cbe1de079b19c6b3357d85f58e0421
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-01-13 17:50:27 +0200

    setup ItemTouchHelper to handle drag and drop

--- app/src/main/java/com/example/recyclerviewdraganddropitem/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdraganddropitem/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.recyclerviewdraganddropitem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private val itemTouchHelper by lazy {
+        val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(UP or DOWN or START or END, 0) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+            }
+        }
+        ItemTouchHelper(itemTouchCallback)
     }
 }
